cffa55cd09bc7625784e5c2a16c1cf9a7ffe4db3
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2016-11-30 13:24:09 +0900

    Step2 version.

--- src/main/jp/ac/uryukyu/ie/e165726/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165726/Main.java
@@ -1,8 +1,13 @@
 package jp.ac.uryukyu.ie.e165726;
 
 public class Main {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException e) {
+            System.out.println("NullPointerException has occurred. ");
+            e.printStackTrace();
+        }
     }
 }
