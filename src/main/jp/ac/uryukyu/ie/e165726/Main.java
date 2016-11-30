7e59b0b5a4631a6d0d91d2fd0a22bf08a53b10e5
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2016-11-30 12:57:34 +0900

    Step1 version.

---
+++ src/main/jp/ac/uryukyu/ie/e165726/Main.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e165726;
+
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
